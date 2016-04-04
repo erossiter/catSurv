377e4bc7838588e8023583b4197a2bdb033669d2
MattMalis <iramalis@nah.dhcp.wustl.edu>
2016-04-04 12:53:28 -0500

    updated grmCat to have all "Cat"s and no "CAT"s

--- catSurv/R/grmCAT.R
+++ catSurv/R/grmCAT.R
@@ -1,12 +1,12 @@
 #' Computerized Adaptive Testing Graded Response Model
 #'
-#' This function fits the Graded Response model for ordinal polytomous data and populates the fitted values for discimination and difficulty parameters to an object of class \code{CATsurv}.  
+#' This function fits the Graded Response model for ordinal polytomous data and populates the fitted values for discimination and difficulty parameters to an object of class \code{Cat}.  
 #'
 #' @param data a \code{data.frame} or a numeric \code{matrix} of manifest variables. 
-#' @param object an object of class \code{CATsurv} to be populated. If omitted, a new object of class \code{CATsurv} is created.
+#' @param object an object of class \code{Cat} to be populated. If omitted, a new object of class \code{Cat} is created.
 #' @param ... arguments to be passed to methods. For more details about the arguments, see \code{\link{grm}}.
 #'
-#'  @return An object of class \code{CATsurv} with components,
+#'  @return An object of class \code{Cat} with components,
 #' \itemize{
 #' \item \code{difficulty} a named list of difficulty parameters for use with polytomous questions/items.  Each element's name tells the question/item to which it applies.
 #' \item \code{guessing} a vector of guessing parameter for each question/item. 
@@ -17,15 +17,15 @@
 #' }
 #' @note In case the Hessian matrix at convergence is not positive definite try to use \code{start.val="random"}. 
 #' @author Josh W. Cutler: \email{josh@@zistle.com} and Jacob M. Montgomery: \email{jacob.montgomery@@wustl.edu}
-#' @seealso \code{\link{ltmCAT}},\code{\link{nextItem}}, \code{\link{question.path}}
-#' @rdname grmCAT
+#' @seealso \code{\link{ltmCat}},\code{\link{nextItem}}, \code{\link{question.path}}
+#' @rdname grmCat
 #' @export
-setGeneric("grmCAT", function(data, object=NULL, ...){standardGeneric("grmCAT")})
+setGeneric("grmCat", function(data, object=NULL, ...){standardGeneric("grmCat")})
 
 #' @export
-setMethod(f="grmCAT", signature="data.frame", 
+setMethod(f="grmCat", signature="data.frame", 
           definition=function(data, object,...){
-            if(!is.null(object)) if(class(object)!="CATsurv") stop("object is not class CATsurv")            
+            if(!is.null(object)) if(class(object)!="Cat") stop("object is not class Cat")            
             fit <- grm(data=data, IRT.param = TRUE,...)
             coefficient <- coef(fit)
             answer <- rep(NA,nrow(coefficient))
@@ -34,7 +34,7 @@ setMethod(f="grmCAT", signature="data.frame",
             names(difficulty) <- rownames(coefficient)
             guessing <- rep(0, length(discrimination))
             if(is.null(object)){
-              return(new("CATsurv", discrimination=discrimination, difficulty=difficulty, poly=TRUE, guessing=guessing, answers=answer))
+              return(new("Cat", discrimination=discrimination, difficulty=difficulty, poly=TRUE, guessing=guessing, answers=answer))
             } else {
               object@discrimination <- discrimination
               object@difficulty <- difficulty
