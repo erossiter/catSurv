a65c9b5bef43d349c39f2a1ca97965acf8d1a335
Alex Weil <alex@alexjweil.com>
2016-04-07 18:49:04 -0500

    Removed unneeded files

--- catSurv/src/Difficulty.cpp
+++
@@ -1,9 +0,0 @@
-#include "Difficulty.h"
-
-const bool Difficulty::is_polytomous() const {
-	return polytomous;
-}
-
-const std::vector<double> &Difficulty::get_difficulty(size_t index) const {
-	return parameters[index];
-}
--- catSurv/src/probability.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "Cat.h"
-
-List probability(S4 cat_df, NumericVector t, IntegerVector q);
