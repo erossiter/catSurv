511b98b641cb47e189842f89170dd56d1b5b903a
MattMalis <iramalis@nah.attlocal.net>
2016-04-11 10:50:46 -0500

    more work on likelihood test

--- catSurv/tests/testthat/test-likelihood.R
+++ catSurv/tests/testthat/test-likelihood.R
@@ -13,22 +13,32 @@ test_that("binary likelihood calculates correctly",{
   ## R test function
   
   likelihood_test <- function(catBi = "Cat", theta = "numeric", items = "numeric"){
+    ## vector of probabilities for each question item
     p_iVec<-sapply(items, function(x){
       probability(catBi, theta, x)
     })
+    ## storing respondent's answers to these question items
     ansVec<-catBi@answers[items]
     
+    ## creating a vector of values inside the product function in equation (3) [from the documentation]
     piqiVec<-sapply(1:length(p_iVec), function(i){
       (p_iVec[i]^ansVec[i])*((1-p_iVec[i])^(1-ansVec[i]))
     })
     
+    ## applying product function over this vector
     likelihood<-sapply(piqiVec, prod) 
+    
     return(likelihood)
   }
-  expect_equal(likelihood(catBi, t=1, q=1), likelihood_test(catBi, 1, 1))
-  expect_equal(likelihood(catBi, t=1872, q=2), likelihood_test(catBi, 1872, 2))
-  expect_equal(likelihood(catBi, t=.001, q=3), likelihood_test(catBi, .001, 3))
-  expect_equal(likelihood(catBi, t=-90, q=4), likelihood_test(catBi, -90, 4))
+  
+  ## NOTE: the documentation says the likelihood functions takes an argument "items", a vector of 
+  ##  the indices of the question items whose answers we want to consider...
+  ## ...the version of the function in main.cpp does not take in an "items" argument, but refers to its 
+  ## "applicable rows" slot instead. I don't know how to resolve this. 
+  expect_equal(likelihood(catBi, t=1), likelihood_test(catBi, 1, 1))
+  expect_equal(likelihood(catBi, t=1872), likelihood_test(catBi, 1872, 2))
+  expect_equal(likelihood(catBi, t=.001), likelihood_test(catBi, .001, 3))
+  expect_equal(likelihood(catBi, t=-90), likelihood_test(catBi, -90, 4))
 })
 
 
