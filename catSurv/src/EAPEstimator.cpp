e76eb9c14e81b02ee5ee3be318638b6368e21586
Alex Weil <alex@alexjweil.com>
2016-04-14 21:57:59 -0500

    Corrected behavior of estimateTheta

--- catSurv/src/EAPEstimator.cpp
+++ catSurv/src/EAPEstimator.cpp
@@ -1,16 +1,42 @@
 #include "EAPEstimator.h"
 
-const double EAPEstimator::estimateTheta(Prior prior) {
-	size_t x_size = questionSet.X.size();
-	std::vector<double> fx(x_size);
-	std::vector<double> fx_x(x_size);
 
-	for (size_t i = 0; i < questionSet.X.size(); ++i) {
-		fx.push_back(likelihood(questionSet.X[i]) * prior.values[i]);
-		fx_x.push_back(questionSet.X[i] * fx[i]);
+template<typename F>
+class gsl_function_pp : public gsl_function {
+public:
+	gsl_function_pp(const F &func) : _func(func) {
+		function = &gsl_function_pp::invoke;
+		params = this;
 	}
 
-	return integrator.integrate(questionSet.X, fx_x) / integrator.integrate(questionSet.X, fx);
+private:
+	const F &_func;
+
+	static double invoke(double x, void *params) {
+		return static_cast<gsl_function_pp *>(params)->_func(x);
+	}
+};
+
+const double EAPEstimator::estimateTheta(Prior prior) {
+	auto numerator_function = [&](double theta, void *params = nullptr) {
+		return theta * likelihood(theta) * prior.prior(theta);
+	};
+
+	auto denominator_function = [&](double theta, void *params = nullptr) {
+		return likelihood(theta) * prior.prior(theta);
+	};
+
+
+	gsl_function_pp<decltype(numerator_function)> Fp(numerator_function);
+	gsl_function *n = static_cast<gsl_function *>(&Fp);
+
+	gsl_function_pp<decltype(denominator_function)> Fp2(denominator_function);
+	gsl_function *d = static_cast<gsl_function *>(&Fp2);
+
+
+	const double numerator = integrator.integrate(n, 10);
+	const double denominator = integrator.integrate(d, 10);
+	return numerator / denominator;
 }
 
 const EstimationType EAPEstimator::getIntegrationType() const {
