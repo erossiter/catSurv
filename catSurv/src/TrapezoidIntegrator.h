7eb0ad64fbd036f07b45f6a66d53f1d4b5bc5f2e
Alex Weil <alex@alexjweil.com>
2016-04-14 21:55:23 -0500

    Removed Trapezoid Integrator for replacement with adaptive quadrature

--- catSurv/src/TrapezoidIntegrator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Integrator.h"
-
-class TrapezoidIntegrator : public Integrator {
-
-public:
-
-	virtual const double integrate(const std::vector<double> &x, const std::vector<double> &fx) const override;
-};
