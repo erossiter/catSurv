451d1e589c8c58b861fa3a2f10418ee6ddf2c5ed
Alex Weil <alex@alexjweil.com>
2016-04-19 13:27:59 -0500

    Created MAP Estimator

---
+++ catSurv/src/MAPEstimator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex Weil on 4/19/16.
+//
+
+#include "MAPEstimator.h"
---
+++ catSurv/src/MAPEstimator.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Estimator.h"
+#include "Prior.h"
+
+
+class EAPEstimator : public Estimator {
+private:
+	constexpr static int integrationSubintervals = 10;
+	typedef std::function<double(double)> integrableFunction;
+
+	/**
+    * Computes the quotient of the integrals of the functions provided
+    * - that is, it computes: ∫(numerator) / ∫(denominator).
+    */
+	double integralQuotient(const integrableFunction &numerator,
+	                        const integrableFunction &denominator);
+
+	double polytomous_posterior_variance(int item, Prior &prior);
+
+
+	double binary_posterior_variance(int item, Prior &prior);
+
+public:
+
+	EAPEstimator(const Integrator &integrator, const QuestionSet &questionSet);
+
+	virtual EstimationType getIntegrationType() const override;
+
+	virtual double estimateTheta(Prior prior) override;
+
+	double estimateSE(Prior prior) override;
+
+	virtual double expectedPV(int item, Prior &prior) override;
+
+};
