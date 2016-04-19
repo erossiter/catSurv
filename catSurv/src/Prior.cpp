be4906038d6f13d977710ad45c3a455139e09200
Alex Weil <alex@alexjweil.com>
2016-04-19 13:26:56 -0500

    Fixed dt to remove manual calculation

--- catSurv/src/Prior.cpp
+++ catSurv/src/Prior.cpp
@@ -1,41 +1,28 @@
+#include <boost/math/special_functions/gamma.hpp>
+#include <boost/math/distributions/chi_squared.hpp>
+#include <boost/math/distributions/students_t.hpp>
 #include "Prior.h"
 
-// double Prior::dchi(double x, double k) {
-//   return pdf(chi_squared(k), x);
-// }
+using namespace boost::math;
+
 double Prior::dchi(double x, double k) {
-  return pdf(chi_squared(x), k);
+	return pdf(chi_squared(x), k);
 }
 
-// double Prior::dt(double x, double mu, int df) {
-// 	double dnorm_value = dnorm4(Rcpp::NumericVector::create(x), 0.0, true, 0)[0];
-// 	return (dchi(x, df) + mu) / std::sqrt(dnorm_value / df);
-// }
-double Prior::dt(double x, double mu, int df) {
-	double dnorm_value = dnorm4(Rcpp::NumericVector::create(x), 0.0, 1.0, 0)[0];
-	return (dnorm_value + mu) / std::sqrt(dchi(x, df) / df);
+double Prior::dt(double x, int df) {
+	return pdf(students_t(df), x);
 }
 
-// double Prior::prior(double x) {
-// 	if (name == "NORMAL") {
-// 		return dnorm4(Rcpp::NumericVector::create(x), parameters[0], (bool) (parameters[1]), 0)[0];
-// 	}
-// 	return dt(x, parameters[0], (int) parameters[2]);
-// }
 double Prior::prior(double x) {
 	if (name == "NORMAL") {
 		return dnorm4(Rcpp::NumericVector::create(x), parameters[0], parameters[1], 0)[0];
 	}
-	return dt(x, parameters[0], (int) parameters[1]);
+	return dt(x, (int) parameters[1]);
 }
 
 Prior::Prior(Rcpp::S4 cat_df) : name(Rcpp::as<std::string>(cat_df.slot("priorName"))),
-                                parameters(Rcpp::as<std::vector<double> >(cat_df.slot("priorParams"))) {
-	for (auto i : (Rcpp::List) cat_df.slot("X")) {
-		values.push_back(prior(i));
-	}
-
-}
+                                parameters(Rcpp::as<std::vector<double> >(cat_df.slot("priorParams"))),
+                                values(Rcpp::as<std::vector<double> >(cat_df.slot("X"))) { }
 
 Prior::Prior(const std::string &prior_name, const std::vector<double> &distribution_parameters) : name(
 		prior_name), parameters(distribution_parameters) { }
--- catSurv/src/Prior.h
+++ catSurv/src/Prior.h
@@ -2,16 +2,12 @@
 #include <string>
 #include <vector>
 #include <Rcpp.h>
-#include <boost/math/special_functions/gamma.hpp>
-#include <boost/math/distributions/chi_squared.hpp>
-
-using namespace boost::math;
 
 class Prior {
 private:
 	double dchi(double x, double k);
 
-	double dt(double x, double mu, int df);
+	double dt(double x, int df);
 
 public:
 	const std::string name;
