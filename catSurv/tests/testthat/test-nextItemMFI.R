21cb22e65caec0c5bcfcdc9c6a322cd1fd310aad
mhseo88 <minheeseo@wustl.edu>
2016-04-28 14:14:45 -0500

    done with these tests. Can't call function fisherInf. So I will try if fisherInf test is done.

--- catSurv/tests/testthat/test-nextItemMFI.R
+++ catSurv/tests/testthat/test-nextItemMFI.R
@@ -8,12 +8,12 @@ test_that("nextItemMFI calculates correctly", {
     unanswered_questions <- which(is.na(cat@answers))
     questions_fisherInf <- rep(0, length(unanswered_questions))
     for(i in 1:length(unanswered_questions)){
-      questions_fisherInf[i] <- fisherInf(cat, unanswered_questions[i])
+      questions_fisherInf[i] <- fisherInf(cat, unanswered_questions[i], theta)
     }
     max_fisher <- max(questions_fisherInf)
     next_item <- which(max(questions_fisherInf))
     return_this <- list(max_fisher, next_item)
     return(return_this)
   }
-  expect_equal(nextItemMFI_test(test_cat), nextItemMFI(test_cat))
+  expect_equal(nextItemMFI_test(testCats[[1]]), nextItemMFI(testCats[[1]]))
 })
--- catSurv/tests/testthat/test-nextitemMLWI.R
+++ catSurv/tests/testthat/test-nextitemMLWI.R
@@ -7,14 +7,14 @@ test_that("nextItemMLWI calculates correctly", {
   nextItemMLWI_test <- function(cat = "Cat"){
     library(stats)
     unanswered_questions <- which(is.na(cat@answers))
-    LWI[i] <- rep(0, length(unanswered_questions))
+    LWI <- rep(0, length(unanswered_questions))
     for(i in 1:length(unanswered_questions)){
-      LWI[i] <- fisherInf(cat, unanswered_questions[i])*likelihood(cat, theta)
+      LWI[i] <- fisherInf(cat, unanswered_questions[i], theta)*likelihood(cat, theta)
     }
     maxLWI <- max(integrate(LWI, -1, 1)$value)
     next_item <- which(max(integrate(LWI, -1, 1)$value))
     return_list <- list(maxLWI, next_item)
     return(return_list)
   }
-  expect_equal(nextItemMLWI_test(test_cat), nextItemMLWI(test_cat))
+  expect_equal(nextItemMLWI_test(testCats[[1]]), nextItemMLWI(testCats[[1]]))
 })
