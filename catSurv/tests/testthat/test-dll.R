a3bf63975461361575253b104638a2bad06c0fc0
mhseo88 <minheeseo@wustl.edu>
2016-04-28 08:45:29 -0500

    done.

--- catSurv/tests/testthat/test-dll.R
+++ catSurv/tests/testthat/test-dll.R
@@ -3,9 +3,39 @@ library(testthat)
 context("dLL")
 
 
-test_that("dLL calculates correctly",{
+test_that("dichotomous case of dLL calculates correctly",{
   
-dLL_test <- function(cat="Cat", theta="numeric", use_prior=TRUE) {
+  dLL_test_bi <- function(cat="Cat", theta="numeric", usePrior=TRUE) {
+    unanswered_questions <- length(is.na(cat@answers))
+    L_theta <- 0
+    if(length(unanswered_questions) == 0) {
+      return_this <- ((theta - cat@priorParams[1]) / cat@priorParams[2]^2)
+      return(return_this)
+    }
+    if(usePrior == FALSE) {
+      for(i in 1:length(unanswered_questions)) {
+        P <- probability(cat, theta, question)
+        Q <- 1-P
+        sum_this <- cat@discrimination[i] * ((P-cat@guessing[i]) / P(1-cat@guessing[i])) * (cat@answers[i]-P)
+        L_theta <- sum_this
+      }
+      return(L_theta)
+    }
+    if(usePrior == TRUE){
+      L_theta <- sum_this - ((theta - cat@priorParams[1])/cat@priorParams[2]^2)
+      return(L_theta)
+    }
+  }
+  expect_equal(dLL(test_cat, 1, TRUE), dLL_test_bi(test_cat, 1, TRUE))
+  expect_equal(dLL(test_cat, 1, FALSE), dLL_test_bi(test_cat, 1, FALSE))
+})
+
+
+
+
+test_that("Graded response case dLL calculates correctly",{
+  
+dLL_test_poly <- function(cat="Cat", theta="numeric", usePrior=TRUE) {
   
   unanswered_questions <- length(is.na(cat@answers))
   L_theta <- 0
@@ -13,19 +43,10 @@ dLL_test <- function(cat="Cat", theta="numeric", use_prior=TRUE) {
     return_this <- ((theta - cat@priorParams[1]) / cat@priorParams[2]^2)
     return(return_this)
   }
-  if(cat@poly == FALSE) {
-    for(i in 1:length(unanswered_questions)-1) {
-      P <- probability(cat, theta, answered_questions[i])
-      Q <- 1-P
-      sum_this <- cat@discrimination[i] * (P-cat@guessing[i] / P(1-cat@guessing[i])) * (cat@answers[i]-P)
-      L_theta <- L_theta + sum_this
-    }
-  }
-  if(cat@poly == TRUE) {
-    for(i in 1:length(unanswered_questions)-1){
-      item <- unanswered_questions[i]
+  if(usePrior == FALSE) {
+    for(i in 1:length(unanswered_questions)){
       answer_k <- cat@answers[i]
-      probs <- probability(cat, theta, answered_questions[i])
+      probs <- probability(cat, theta, question)
       Pstar1 <- probs[answer_k]
       Qstar1 <- 1-Pstar1
       Pstar2 <- probs[answer_k -1]
@@ -33,15 +54,18 @@ dLL_test <- function(cat="Cat", theta="numeric", use_prior=TRUE) {
       P <- Pstar2 - Pstar1
       W2 <- Pstar2 * Qstar2
       W1 <- Pstar1 * Qstar1
-      L_theta <- L_theta + (cat@discrimintation[item] * ((W2 - W1)/P))
+      sum_this <- cat@discrimintation[i] * ((W2 - W1)/P)
     }
+    L_theta <- sum_this
+    return(L_theta)
   }
   if(usePrior == TRUE){
-    L_theta <- L_theta - ((theta - cat@priorParams[1])/cat@priorParams[2]^2)
+    L_theta <- sum_this - ((theta - cat@priorParams[1])/cat@priorParams[2]^2)
+    return(L_theta)
   }
-  return(L_theta)
+  expect_equal(dLL(test_cat, 1, TRUE), dLL_test_poly(test_cat, 1, TRUE))
+  expect_equal(dLL(test_cat, 1, FALSE), dLL_test_poly(test_cat, 1, FALSE))
 }
-  expect_equal(dLL(cat_df=allTheCats, theta=1, TRUE), dLL_test(cat=test_cat1, theta=1, TRUE))
-  expect_equal(dLL(cat_df=allTheCats, theta=1, use_prior=FALSE), dLL_test(test_cat, 1, FALSE))
-})
+}
+)
 
