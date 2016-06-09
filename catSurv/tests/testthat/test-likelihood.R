36c33ef3df8d764097a6e84a1004712542d560f2
RydenButler <ryden.winfield@gmail.com>
2016-06-09 16:59:23 -0500

    Revise Probability/Likelihood Tests
    Reduce redundancies in probability/likelihood tests; combine
    binary/polytomous tests into single, conditional test

--- catSurv/tests/testthat/test-likelihood.R
+++ catSurv/tests/testthat/test-likelihood.R
@@ -1,54 +1,68 @@
 library(catSurv)
 context("Likelihood")
 
-########## BINARY LIKELIHOOD TEST ##############
-
-test_that("binary likelihood calculates correctly", {
+test_that("likelihood calculates correctly", {
   
-  ## creating new Cat object and filling in the slots
-  allTheCats<-catBiCreator(10, fillAnswers=.3)
-  
-  ## creating vector of thetas
-  setThetas<-function(seed="numeric"){
-    set.seed(seed)
-    thetas<-c(2*rnorm(length(allTheCats))) # drawing one theta value for each Cat (number of draws = length(allTheCats))... 
-    ## ...and multiplying by 2 so the values cover a range of ~(-4,4)
-  }
-  thetaVec<-setThetas(1000)
-  
-  
-  ## R test function
-  
-  likelihood_test_bi <- function(catBi = "Cat", theta = "numeric"){
-    ## vector of probabilities for each question item that has been answered
-    answered_indices<-which(!is.na(catBi@answers), arr.ind=T)
+  # R test function
+  likelihood_test <- function(cat = "Cat", theta = "numeric"){
     
-    if(length(answered_indices)>0){
+    # Identifies answered questions and combines them in vector
+    answered_indices <- which(!is.na(cat@answers), arr.ind = T)
+    ansVec <- cat@answers[answered_indices]
+    
+    # Create list of probability vectors for each question
+    pList <- lapply(answered_indices, function(i) {
+      probability(cat, theta, i)$all.probabilities$probabilities
+    })
+    
+    if (length(answered_indices) > 0) {
       
-      p_iVec<-sapply(answered_indices, function(i){
-        probability(catBi, theta, i)$all.probabilities$probabilities
-      })
-      ## storing respondent's answers to these question items
-      ansVec<-catBi@answers[answered_indices]
+      # For binary likelihood
+      if (cat@poly == F) {
+        # Creates a vector of values inside the product function in equation (3) [from the documentation]
+        pqVec<-sapply(1:length(pList), function(i) {
+          (pList[[i]]^(ansVec[i])) * ((1 - pList[[i]])^(1 - ansVec[i]))
+        })
+        # Apply product function over this vector
+        likelihood <- prod(pqVec) 
+      } 
       
-      ## creating a vector of values inside the product function in equation (3) [from the documentation]
-      piqiVec<-sapply(1:length(p_iVec), function(i){
-        (p_iVec[i]^(ansVec[i]))*((1-p_iVec[i])^(1-ansVec[i]))
-      })
-      ## applying product function over this vector
-      likelihood<-prod(piqiVec) 
+      #  For polytomous likelihood
+      if (cat@poly == T) {
       
+      pListFinal <- vector("list", length(pList))
+      for (i in 1:length(pList)) {  # Iterating over question
+        # Append 0 and 1 to front and back or each vector of probabilties
+        pList[[i]] <- c(0, pList[[i]], 1)
+        for (k in 2:length(pList[[i]])) { #  iterating over response categories
+          pListFinal[[i]][k - 1] <- pList[[i]][k] - pList[[i]][k - 1]
+        }
+      }
+      
+      probExpList <- pListFinal #  Copying for dimensions
+      for (i in 1:length(probExpList)) {
+        for (k in 1:length(probExpList[[i]])) {
+          probExpList[[i]][k] <- (pListFinal[[i]][k])^(ansVec[i] == k)
+        }
+      }
+      
+      # Multiplying over response categories
+      productVec <- sapply(probExpList, prod)
+      # Multiplying over question items
+      likelihood <- prod(productVec)
+      } 
       return(likelihood)
-    }
-    else return (1)
+    } else return (1)
   }
   
-  ## applying real likelihood function on my cats
+  ##### Note that the following functions require data inputs before test_that can operate
+  
+  # Applying real likelihood function on my cats
   realFunValues<-lapply(1:length(allTheCats), function(x){
     likelihood(allTheCats[[x]], thetaVec[x])
   })
   
-  ## applying test likelihood function on my cats  
+  # Applying test likelihood function on my cats  
   
   testFunValues<-lapply(1:length(allTheCats), function(x){
     likelihood_test_bi(allTheCats[[x]], thetaVec[x])
@@ -56,111 +70,5 @@ test_that("binary likelihood calculates correctly", {
   
   expect_equal(realFunValues, testFunValues)
   
-  
-})
-
-rm(list=ls())
-
-
-
-
-
-
-
-
-
-########## POLYTOMOUS LIKELIHOOD TEST ##############
-
-test_that("polytomous likelihood calculates correctly",{
-  
-  ## creating lots of new Cats  and filling in the slots
-  
-  allTheCats<-catPolyCreator(10, fillAnswers=0.4, seed=9576)
-  
-  ## creating vector of theta values
-  setThetas<-function(seed=18){
-    set.seed(seed)
-    thetas<-c(2*rnorm(length(allTheCats))) # drawing one theta value for each Cat (number of draws = length(allTheCats))... 
-    ## ...and multiplying by 2 so the values cover a range of ~(-4,4)
-  }
-  thetaVec<-setThetas()
-  
-  
-  ## R test function
-  ## this function runs on a single Cat object, given a single theta value
-  
-  likelihood_test_poly <- function(catPoly = "Cat", theta = "numeric"){
-    ##identifying indices of question items that have been answered
-    ## (equivalent of "applicable rows" in cpp)
-    answered_indices<-which(!is.na(catPoly@answers), arr.ind=T)
-    
-    ##storing answers for question items that have been answered
-    ansVec<-catPoly@answers[answered_indices]
-    
-   
-    
-    ## only calculate likelihood if at least one question has been answered...
-    ## (if no questions have been answered, likelihood is 1)
-    
-    if(length(answered_indices)>0){
-      
-      ## each element in p_ikList is a vector (of variable length, possibly length 1) corresponding to a question item 
-      ##    (the output of the probability function for each question item)
-      ## each vector will be of length (k_i - 1), where k_i is the number of possible response
-      ##    categories to question i (and thus (k_i - 1) is the number of difficulty parameters for that question item), 
-      ##    and each value in the vector is the probability of the respondent giving a response in a category strictly higher than k
-      
-      p_ikList<-lapply(answered_indices, function(i){
-        probability(catPoly, theta, i)$all.probabilities$probabilities
-      })
-      
-      ## now, need to convert each value in each vector...
-      ##  ...from "the probability of a response in a category strictly higher than k"...
-      ##  ...to, "the probability of a response in exactly category k"
-      
-      #p_ikListExact<-p_ikList ##copy list, for dimensions
-      
-      p_ikListExact <- vector("list", length(p_ikList))
-      for (i in 1:length(p_ikList)){ ##iterating over question
-        p_ikList[[i]]<-c(0,p_ikList[[i]],1) ## append one response category slot to the end of each question item's vector of probabilities
-        ## BECAUSE: the probability values are "the probability of a response in category strictly higher than k"
-        ## so, the probability vectors did not include a value for category k_i: a response could be in exactly category
-        ## k_i, but never a category higher than k_i, so that value would have been zero
-        ## BUT, here we want to include the probability of a response in category EXACTLY k_i
-        for(k in 2:length(p_ikList[[i]])){ ##iterating over response categories
-              p_ikListExact[[i]][k-1] <- p_ikList[[i]][k] - p_ikList[[i]][k-1]
-        }
-      }
-      
-      
-      ## creating a list of vectors of (P_ijk)^I(y_ij = k) values... 
-      ## ... each element of the list is a vector, corresponding to question item i, of length k_i
-      ## ... and each element of each (length k_i) vector is the value of (P_ijk)^I(y_ij = k)
-      probExpList<-p_ikListExact ##copying, for dimensions
-      for(i in 1:length(probExpList)){
-        for(k in 1:length(probExpList[[i]])){
-          probExpList[[i]][k]<-(p_ikListExact[[i]][k])^(ansVec[i]==k)
-        }
-      }
-      
-      ##multiplying over response categories
-      productVec<-sapply(probExpList, prod)
-      ##multiplying over question items
-      likelihood<-prod(productVec)
-      return(likelihood)
-      }
-    else return(1)
-  }
-  
-  ### running real and test functions on allTheCats
-  realFunValues<-lapply(1:length(allTheCats), function(x){
-    likelihood(allTheCats[[x]], thetaVec[x])
-  })
-  
-  testFunValues<-lapply(1:length(allTheCats), function(x){
-    likelihood_test_poly(allTheCats[[x]], thetaVec[x])
-  })
-  
-  expect_equal(realFunValues, testFunValues)
   
 })
--- catSurv/tests/testthat/test-probability.R
+++ catSurv/tests/testthat/test-probability.R
@@ -1,42 +1,33 @@
 library(catSurv)
 context("Probability")
-rm(list=ls())
-###### BINARY PROBABILITY TEST ########
 
-test_that("binary probability calculates correctly", {
+test_that("probability calculates correctly", {
   
-  ## Creating a lot of cat objects and filling in needed slots
-  ## ADJUST THIS INPUT IF YOU WANT A SHORTER OR LONGER TEST
-  allTheCats<-catBiCreator(10,seed=7896)
-
-  ## setting the question and theta values for each Cat, to be used in the probability function...
-  
-  setThetas<-function(seed="numeric"){
-    set.seed(seed)
-    thetas<-c(2*rnorm(length(allTheCats))) # drawing one theta value for each Cat (number of draws = length(allTheCats))... 
-    ## ...and multiplying by 2 so the values cover a range of ~(-4,4)
-  }
-  thetaVec<-setThetas(1000)
-  
-  set.seed(2222)
-  questionList<-lapply(allTheCats, function(x){
-    #drawing a question/item number randomly from the number of questions stored in each Cat:
-    ##  (number of quesitons corresponds to the length of a Cat's discrimination vector)
-    return(sample(length(x@discrimination), 1))
-  })
-  questionVec<-unlist(questionList)
-  
-
-  ## R test function
-  probability_test_bi <- function(cat = "Cat", theta = "numeric", question = "numeric"){
+  # R test function
+  probability_test <- function(cat = "Cat", theta = "numeric", question = "numeric"){
     discrimination = cat@discrimination[question]
-    difficulty = cat@difficulty[question]
+    difficulty = cat@difficulty[[question]]
     guessing = cat@guessing[question]
+    
+    # For binary probability
+    if (cat@poly == F) {
     exp_prob = exp(difficulty + (theta * discrimination))
     probability <- guessing + (1-guessing) * (exp_prob / (1 + exp_prob))
+    
+    # For polytomous probability
+    } else {
+      probability <- rep(NA,length(difficulty))
+      for(k in 1:length(difficulty)){
+        exp_prob = exp((difficulty[k]) - (theta * discrimination))
+        probK <- (exp_prob/(1+exp_prob))
+        probability[k] <- probK
+      }
+    }
     return(probability)
   }
 
+  ##### Note that the following functions require data inputs before test_that can operate
+  
   ##calculating values from real probability function (found in Estimator.cpp)
   realFunValues<-lapply(1:length(allTheCats), function(x){
     return(as.numeric((probability(allTheCats[[x]], thetaVec[x], questionVec[x]))$all.probabilities))
@@ -51,74 +42,6 @@ test_that("binary probability calculates correctly", {
   
   ##expect the values to be equal
   expect_equal(realFunValues, testFunValues)
-  
-})
-
-rm(list=ls())
-
-
-
-
-###### POLYTOMOUS PROBABILITY TEST ########
-
-test_that("polytomous probability calculates correctly", {
-  
-
-  ## creating lots of polytomous cats
- 
-  allPolyCats<-catPolyCreator(12, seed=9872453)
-  
-  for (i in 1:length(allPolyCats)){
-    print(allPolyCats[[i]]@difficulty)
-  }
-
-  ## R test function
-  probability_test_poly <- function(cat = "Cat", theta = "numeric", question = "numeric"){
-    discrimination = cat@discrimination[question]
-    difficulty = cat@difficulty[[question]]
-    probVec <- c()
-    for(k in 1:length(difficulty)){
-      exp_prob = exp((difficulty[k]) - (theta * discrimination))
-      probK <- (exp_prob/(1+exp_prob))
-      probVec <- c(probVec, probK)
-    }
-    return((probVec))
-  }
-  
-  ## creating theta values and question numbers, as inputs for the probability function 
-  ##  (one theta and one question number for each Cat)
-  
-  setThetas<-function(spread=2, seed=754){
-    set.seed(seed)
-    return(spread*rnorm(length(allPolyCats))) # drawing one theta value for each Cat (number of draws = length(allPolyCats))... 
-    ## ...and multiplying by a spread factor so the values cover a certain range
-  }
-  thetaVec<-setThetas()
-  
-  questionList<-lapply(allPolyCats, function(x, seed=2534){
-    set.seed(seed)
-    #drawing a question randomly from the number of questions stored in each Cat:
-    ##  (number of quesitons corresponds to the length of a Cat's discrimination vector)
-    return(sample(length(x@discrimination), 1))
-  })
-  questionVec<-unlist(questionList)
-  
-  
-  ##calculating values from real probability function 
-  realFunValues<-lapply(1:length(allPolyCats), function(x){
-    return(probability(allPolyCats[[x]], thetaVec[x], questionVec[x])$all.probabilities$probabilities)
-  })
-
-
-  
-  ##calculating values from the test probability function (created above)
-  testFunValues<-lapply(1:length(allPolyCats), function(x){
-    probability_test_poly(allPolyCats[[x]], thetaVec[x], questionVec[x])
-  })
-  
-  
-  expect_equal(realFunValues, testFunValues)
-  
   
 })
 
