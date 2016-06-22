dc80624f662c72718d3d4ddfea2e643bdd45c108
RydenButler <ryden.winfield@gmail.com>
2016-06-22 16:44:49 -0500

    Added extensive commentary to MLE_Testing
    Added relevant comments; demarcated sections;
    cleaned code so each section can be reproduced separately

--- MLE_Testing.R
+++ MLE_Testing.R
@@ -1,63 +1,106 @@
 library(ltm)
 
+##########################################
+##########################################
+# Binary Weighted MLE
+#########################################
+#########################################
+
+# Subsets a manageable amount of binary data;
+#    creates an ltm object and estimates its factor scores (thetas)
+#    creates a cat with relevant parameters and defines answers
 data.ltm <- npi[1:100,]
+
 trial.ltm <- ltm(data.ltm ~ z1, control = list(GHk = 100))
 fscores <- factor.scores.ltm(trial.ltm)$score.dat
 
 trialCat <- ltmCat(object = trial.ltm)
 trialCat@answers <- as.numeric(as.vector(fscores[1,1:(dim(fscores)[2] - 4)]))
 
-#catIrt
+##############################
+# catIrt Estimate - Binary
+#############################
+
 library(catIrt)
-response <- trialCat@answers
 parameters <- matrix(c(coef(trial.ltm)[,2], coef(trial.ltm)[,1], trialCat@guessing),
                  ncol=3, byrow = F)
 
-wleEst(resp = response, params = parameters, range = c(-6,6), mod = "brm")$theta
-# -0.5483
+wleEst(resp = trialCat@ answers, params = parameters, range = c(-6,6), mod = "brm")$theta
+# result: -0.5483
+
 detach(package:catIrt)
 
-#catR
+###########################
+# catR Estimate - Binary
+##########################
+
 library(catR)
+
 it <- matrix(c(coef(trial.ltm)[,2], coef(trial.ltm)[,1], 
                rep(0,length(trialCat@discrimination)), 
                rep(1, length(trialCat@discrimination))),
             ncol= 4, byrow = F)
 
-thetaEst(it = it, x = response, method = "WL", #D = 1,
+thetaEst(it = it, x = trialCat@ answers, method = "WL", #D = 1,
          priorDist = "norm", priorPar = c(0,1), range = c(-10,10))
-# -0.5482474
+# result: -0.5482691
+
 detach(package:catR)
 
-#catSurv
-MLtest.bi <- function(cat) {
+
+###################################################
+# Creating a binary WLE function in R for catSurv <------------------  convert to C++
+##################################################
+
+WLtest.bi <- function(cat) {
   
+# An arbitrary function to calculate W because
+#    uniroot requires its input be a function
   W.bi <- function(cat, theta){
     a <- cat@difficulty
     b <- cat@discrimination
     c <- cat@guessing
   
+    # Calculate probability + its first & second derivatives
     Pr <- c + (1 - c) * plogis(a + (b * theta))
     dPr <- (b * (1 - c) * exp(a + b * theta)) / (1 + exp(a + b * theta))^2
     d2Pr <- (b^2 * exp(a + b * theta)*(1 - exp(a + b * theta)) * (1 - c))/(1 + exp(a + b * theta))^3
+    
+    # Defines relevant components of W; calculates W
     B <- sum((dPr * d2Pr) / (Pr * (1 - Pr)))
-  
-    W <- dLL(cat, theta, 0) + (B / (2 * sum(sapply(1:length(cat@answers), fisherInf, cat_df = cat, theta = theta))))
+    FisherSum <- sum(sapply(1:length(cat@answers), fisherInf, cat_df = cat, theta = theta))
+    dLogLik <- dLL(cat, theta, 0)
+
+    W <- dLogLik  + (B / (2 * FisherSum))
     return(W)
 }
-  
+
+# uniroot requires a function in terms of the variable of interest
 f <- function(theta) W.bi(cat, theta)
-MLE <- uniroot(f, c(-10,10), extendInt = "yes")$root
-return(MLE)
+
+# uniroot is finicky with its range. 
+#   Too large and it throws an error. 
+#   Too small and it fails to find a root.
+#   Setting a small range while passing extendInt = "yes" takes care of the problem.
+#   It searches for roots outside the range if necessary.
+#   Hopefully such maneuvering isn't necessary for the GSL equivalent
+WLE <- uniroot(f, c(-4,4), extendInt = "yes")$root
+return(WLE)
 }
-MLtest.bi(trialCat) # -0.5482474
+WLtest.bi(trialCat) # result: -0.5482474
+
+###################################################
+# Comparing estimates against catR 
+##################################################
+
+library(catR)
 
 estimates.bi.catSurv <- rep(NA,dim(fscores)[1])
 estimates.bi.catR <- rep(NA,dim(fscores)[1])
 for (i in 1:dim(fscores)[1]) {
   Cat.bi <- trialCat
   Cat.bi@answers <- as.numeric(as.vector(fscores[i,1:(dim(fscores)[2] - 4)]))
-  estimates.bi.catSurv[i] <- MLtest.bi(Cat.bi)
+  estimates.bi.catSurv[i] <- WLtest.bi(Cat.bi)
   
   estimates.bi.catR[i] <- thetaEst(it = it, x = Cat.bi@answers, method = "WL",
            priorDist = "norm", priorPar = c(0,1), range = c(-10,10))
@@ -65,17 +108,29 @@ for (i in 1:dim(fscores)[1]) {
 
 comparison.bi <- data.frame(estimates.bi.catSurv, estimates.bi.catR)
 comparison.bi$differences <- abs(comparison.bi[,1] - comparison.bi[,2])
-colnames(comparison.bi) <- c("MLtest", "thetaEst", "differences")
+colnames(comparison.bi) <- c("WLtest", "thetaEst", "differences")
 comparison.bi
 summary(comparison.bi)
 
+detach(package:catR)
 
 
 
 
-############
-#~~~~Poly~~~~
-############
+
+
+
+
+
+##########################################
+##########################################
+# Categorical Weighted MLE
+#########################################
+#########################################
+
+# Subsets a manageable amount of polytomous data;
+#    creates a grm object and estimates its factor scores (thetas)
+#    creates a cat with relevant parameters and defines answers
 
 data.grm <- nfc[1:100,]
 trial.grm <- grm(data.grm, control = list(GHk = 100))
@@ -84,49 +139,80 @@ fscores.grm <- factor.scores.grm(trial.grm)$score.dat
 trialCat.grm <- grmCat(object = trial.grm)
 trialCat.grm@answers <- as.numeric(as.vector(fscores.grm[1,1:(dim(fscores.grm)[2] - 4)]))
 
-#catIrt
+#################################
+# catIrt Estimate - Categorical
+################################
+
 library(catIrt)
-response.grm <- trialCat.grm@answers
+
 parameters.grm <- matrix(c(coef(trial.grm)[,5], coef(trial.grm)[,1:4]),
                      ncol=5, byrow = F)
 
-wleEst(resp = response.grm, params = parameters.grm, range = c(-6,6), mod = "grm")$theta
-# 0.0471
+wleEst(resp = trialCat.grm@answers, params = parameters.grm, range = c(-6,6), mod = "grm")$theta
+# result: 0.0471
+
 detach(package:catIrt)
 
-#catR
+#################################
+# catR Estimate - Categorical
+################################
+
 library(catR)
+
 it.grm <- matrix(c(coef(trial.grm)[,5], coef(trial.grm)[,1:4]),
              ncol= 5, byrow = F)
 
-thetaEst(it = it.grm, x = (response.grm - 1), model = "GRM", method = "WL", #D = 1,
+thetaEst(it = it.grm, x = (trialCat.grm@answers - 1), model = "GRM", method = "WL", #D = 1,
          priorDist = "norm", priorPar = c(0,1), range = c(-10,10))
-# 0.04710347
+# result: 0.04710441
+
 detach(package:catR)
 
+#########################################################
+# Creating a categorical WLE function in R for catSurv <------------------  convert to C++
+########################################################
 
-MLtest.poly <- function(cat) {
-  
+WLtest.poly <- function(cat) {
+
+# An arbitrary function to calculate W because
+#    uniroot requires its input be a function
   W.poly <- function(cat, theta){
     a <- cat@difficulty
     b <- cat@discrimination
     c <- cat@guessing
     resp <- cat@answers
     
+    
     B_j <- rep(NA, length(resp))
     for(j in 1:length(resp)){
-      P_stars <- c(0, as.vector(unlist(probability(cat, theta, j))), 1)
-      P_stars <- 1 - P_stars
       
+      # Create vector of each p* per question
+      P_stars <- c(0, as.vector(unlist(probability(cat, theta, j))), 1)
+      
+      # Invert p* values such that they decrease from 1...0 and p* values are listed as 1-p*
+      P_stars <- 1 - P_stars
+      # This step was done to mirror catR's method, as
+      # This requires ***UPDATING THE DOCUMENTATION***
+      # However, because the p* value are inverted, the equations for dP and d2P
+      #   remain correct in the current documentation.
+      # I suspect that the function will still work if the original order is preserved, 
+      #   so long as corrections are made to dP & d2P, 
+      #   as well as to their equations in the documentation.
+      
+      # Calculate a probability estimate for each interval demarcated by P*
       P <- P_stars[-1]
       for(k in 1:(length(P_stars) - 1)){
         P[k] <- P_stars[k] - P_stars[k+1]
       }
       
+      # First & second derivaties of P* 
       dP_stars <- b[j] * P_stars * (1 - P_stars)
       
       d2P_stars <- b[j] * (dP_stars - (2 * P_stars * dP_stars))
+      # Note that the documentation currently shows -b as the leading coefficient
+      #   for both of the above derivatives. This requires ***UPDATING THE DOCUMENTATION***
       
+      # First & second derivatives  of P   
       dP <- dP_stars[-1]
       for(k in 1:(length(dP_stars) - 1)){
         dP[k] <- dP_stars[k] - dP_stars[k+1]
@@ -137,28 +223,48 @@ MLtest.poly <- function(cat) {
        d2P[k] <- d2P_stars[k] - d2P_stars[k+1]
       }
       
+      # Sum for each question
       B_j[j] <- sum((dP * d2P)/P)
     }
     
+    # Sum across all questions
     B <- sum(B_j)
     
-    W <- dLL(cat, theta, 0) + (B / (2 * sum(sapply(1:length(cat@answers), fisherInf, cat_df = cat, theta = theta))))
+    # Defines other relevant components of W; calculates W
+    FisherSum <- sum(sapply(1:length(cat@answers), fisherInf, cat_df = cat, theta = theta))
+    dLogLik <- dLL(cat, theta, 0)
+    
+    W <- dLogLik  + (B / (2 * FisherSum))
     return(W)
   }
-  
+  # uniroot requires a function in terms of the variable of interest
   f <- function(theta) W.poly(cat, theta)
-  MLE <- uniroot(f, c(-4,4), extendInt = "yes")$root
-  return(MLE)
+  
+  # uniroot is finicky with its range. 
+  #   Too large and it throws an error. 
+  #   Too small and it fails to find a root.
+  #   Setting a small range while passing extendInt = "yes" takes care of the problem.
+  #   It searches for roots outside the range if necessary.
+  #   Hopefully such maneuvering isn't necessary for the GSL equivalent
+  WLE <- uniroot(f, c(-4,4), extendInt = "yes")$root
+  return(WLE)
 }
-MLtest.poly(trialCat.grm)  
-# 0.04692087
+WLtest.poly(trialCat.grm)  
+# result: 0.04692087
+
+
+###################################################
+# Comparing estimates against catR 
+##################################################
+
+library(catR)
 
 estimates.poly.catSurv <- rep(NA,dim(fscores.grm)[1])
 estimates.poly.catR <- rep(NA,dim(fscores.grm)[1])
 for (i in 1:dim(fscores.grm)[1]) {
   Cat.poly <- trialCat.grm
   Cat.poly@answers <- as.numeric(as.vector(fscores.grm[i,1:(dim(fscores.grm)[2] - 4)]))
-  estimates.poly.catSurv[i] <- MLtest.poly(Cat.poly)
+  estimates.poly.catSurv[i] <- WLtest.poly(Cat.poly)
   
   estimates.poly.catR[i] <- thetaEst(it = it.grm, x = (Cat.poly@answers - 1), model = "GRM", method = "WL",
                                    priorDist = "norm", priorPar = c(0,1), range = c(-10,10))
@@ -166,9 +272,11 @@ for (i in 1:dim(fscores.grm)[1]) {
 
 comparison.poly <- data.frame(estimates.poly.catSurv, estimates.poly.catR)
 comparison.poly$differences <- abs(comparison.poly[,1] - comparison.poly[,2])
-colnames(comparison.poly) <- c("MLtest", "thetaEst", "differences")
+colnames(comparison.poly) <- c("WLtest", "thetaEst", "differences")
 comparison.poly
 summary(comparison.poly)
+
+detach(package:catR)
 
 
 
