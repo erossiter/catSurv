0f8bf0fc6a081f5cfbdfc79eac3c4181a1584cd7
MattMalis <iramalis@nah.dhcp.wustl.edu>
2016-07-18 15:42:53 -0500

    iic and icc plotting functions working for bi and poly cats

--- catSurv/R/iccPlots.R
+++ catSurv/R/iccPlots.R
@@ -9,7 +9,7 @@
 #'  @return A list with components,
 #' \itemize{
 #' \item \code{iccPlot} a named vector of difficulty parameters for use with dichotomous questions/items.  Each element's name tells the question/item to which it applies.
-#' \item \code{x} A matrix of probabilities calculated at each theta from which the curves were drawn.
+#' \item \code{iccInfo} A matrix of theta values, and the probabilities calculated at each theta value from which the curves were drawn.
 #'}
 #' @rdname icc
 #' @export
@@ -18,12 +18,14 @@ setGeneric("icc", function(object, theta_range = seq(-3, 3, .1), question, ...){
 #' @export
 setMethod(f="icc", signature="Cat",
           definition=function(object, theta_range, question, ...){
-            if(!is.null(object)) if(class(object)!="Cat") stop("object is not class Cat")
+            
+            if(is.null(object)) stop("object is null!")
+            if(class(object)!="Cat") stop("object is not class Cat")
             
             prob_given_theta <- function(i){
               probability(object, theta_range[i], question)$all.probabilities$probabilities
             }
-            x <- sapply(1:length(theta_range), prob_given_theta)
+            probs <- sapply(1:length(theta_range), prob_given_theta)
             
             plot.new()
             return_plot <- recordPlot()
@@ -31,15 +33,24 @@ setMethod(f="icc", signature="Cat",
             title(main = paste0("Item Characteristic Curves for Question ", question),
                   xlab = "Theta",
                   ylab = "Probability")
-            axis(1, at = seq(-3,3,1), labels = seq(-3,3,1))
+            axis(1, at = seq(floor(min(theta_range)),ceiling(max(theta_range)),1), labels = seq(floor(min(theta_range)),ceiling(max(theta_range)),1))
             axis(2, at = seq(0,1,.25), labels = seq(0,1,.25), las = 1)
-            legend("topright", c(paste0("Item ", 1:(nrow(x)+1))), lty = 1:(nrow(x)+1), bty = "n")
-            
-            lines(x = theta_range, y = (x[1, ] - 0), lty = 1)
-            lines(x = theta_range, y = (1 - x[nrow(x), ]), lty = nrow(x)+1)
-            for(i in 2:(nrow(x))){
-              lines(x = theta_range, y = (x[i, ] - x[(i-1), ]), lty = i)
+            if(object@poly){
+              legend(x=1.6,y=.9, legend=c(1:(nrow(probs)+1)), lty = 1:(nrow(probs)+1), bty = "n", cex=.7)
+              legend(x=1.4, y=1, legend="Response\nCategory",bty = "n",cex=.7)
+              lines(x = theta_range, y = (probs[1, ] - 0), lty = 1)
+              lines(x = theta_range, y = (1 - probs[nrow(probs), ]), lty = nrow(probs)+1)
+              for(i in 2:(nrow(probs))){
+                lines(x = theta_range, y = (probs[i, ] - probs[(i-1), ]), lty = i)
+                iccInfo<-cbind(theta_range,t(probs))
+              }
             }
-            return(x)
+            else{
+              legend(x=.7,y=.9,"Probability of correct\nresponse given theta",bty='n', cex=.7)
+              lines(x=theta_range, y=probs)
+              iccInfo<-cbind(theta_range,probs)
+            }
+            
+            return(iccInfo)
           })
           
--- catSurv/R/iicPlots.R
+++ catSurv/R/iicPlots.R
@@ -14,34 +14,54 @@
 #'}
 #' @rdname iic
 #' @export
-setGeneric("iic", function(object, theta_range = seq(-5, 5, .1), question, ...){standardGeneric("iic")})
+setGeneric("iic", function(object, theta_range = seq(-3, 3, .1), question, ...){standardGeneric("iic")})
 
 #' @export
 setMethod(f="iic", signature="Cat",
           definition=function(object, theta_range, question, ...){
+            
+            if(is.null(object)) stop("object is null!")
+            if(class(object)!="Cat") stop("object is not class Cat")
+            
             fishers <- sapply(theta_range, function(i){
               return(fisherInf(object, i, question))
             })
             
             plot.new()
             return_plot <- recordPlot()
-            plot.window(c(min(theta_range), max(theta_range)), c(0,1.1*max(fishers)))
+            if(max(fishers)>100) maxFish=100
+            else maxFish=max(fishers)
+            
+            plot.window(c(min(theta_range), max(theta_range)), c(0, 1.1*maxFish))
             title(main = paste0("Item Information Curves for Question ", question),
                   xlab = "Theta",
                   ylab = "Fisher Information")
             axis(1, at = seq(min(theta_range), max(theta_range),1), labels = seq(min(theta_range), max(theta_range),1))
-            if(max(fishers)>=0.5){
-              axis(2, at = seq(0, 1.1*max(fishers), .2), labels = seq(0,1.1*max(fishers), .2), las = 1)
-            }
-             else axis(2, at = seq(0,max(fishers), .05), labels = seq(0,max(fishers), .05), las = 1)
+            ifelse(maxFish<0.5,
+                   ifelse(maxFish<.05,
+                      axis(2, at = c(0,signif(maxFish,1)), labels = c(0,signif(maxFish,1)),las=1), 
+                      axis(2, at = seq(0,maxFish, .05), labels = seq(0,maxFish, .05), las = 1)),
+                   ifelse(maxFish<2,
+                      axis(2, at = seq(0, 1.1*max(fishers), .2), labels = seq(0,1.1*max(fishers), .2), las = 1),
+                      axis(2, at = seq(0, 1.1*max(fishers), .5), labels = seq(0,1.1*max(fishers), .5), las = 1)))
+                      
+#             if (object@poly){
+#               legend(x=1.6,y=.9, legend=c(1:(nrow(fishers)+1)), lty = 1:(nrow(fishers)+1), bty = "n", cex=.7)
+#               legend(x=1.4, y=1, legend="Response\nCategory",bty = "n",cex=.7)
+#               #lines(x = theta_range, y = (probs[1, ] - 0), lty = 1)
+#               #lines(x = theta_range, y = (1 - probs[nrow(probs), ]), lty = nrow(probs)+1)
+#               for(i in 1:(nrow(fishers))){
+#                 lines(x = theta_range, y = (probs[i, ] - probs[(i-1), ]), lty = i)
+#                 iccInfo<-cbind(theta_range,t(probs))
+#               }
 #             
-#              if(max(fishers)>=0.5){
-#               axis(2, at = seq(0,max(fishers), signif((max(fishers)/5),2)), labels = seq(0,max(fishers), signif((max(fishers)/5),1)), las = 1)
 #             }
-#             else axis(2, at = seq(0,max(fishers), signif((max(fishers)/5),2)), labels = seq(0,max(fishers), signif((max(fishers)/5),2)), las = 1)
-#             #legend("topright", c(paste0("Item ", 1:(nrow(x)+1))), lty = 1:(nrow(x)+1), bty = "n")
             
-            lines(x = theta_range, y = fishers, lty = 1)
+            #else {
+              lines(x = theta_range, y = fishers, lty = 1)
+              legend("topright", paste("Fisher Information\ngained by asking\nquestion",question,"\ngiven theta"),bty='n',cex=.6)
+              
+           # }
             #lines(x = theta_range, y = (1 - x[nrow(x), ]), lty = nrow(x)+1)
             #for(i in 2:(nrow(x))){
             #  lines(x = theta_range, y = (x[i, ] - x[(i-1), ]), lty = i)
