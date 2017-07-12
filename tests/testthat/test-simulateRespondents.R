ed7b1d661f3bd50d61259e8edc3f9875e3d93a7b
erossiter <erinrossiter@psci017als17.wustl.edu>
2017-07-12 15:07:30 -0500

    Added R version of two new functions
    simulateRespondents() and oracle() are now included in the tests folder
    for c++ implementation to be based off of

---
+++ tests/testthat/test-simulateRespondents.R
@@ -0,0 +1,54 @@
+context("estimateThetas")
+load("cat_objects.Rdata")
+
+simulateRespondents_test <- function(cat, theta, n){
+  if(sum(!is.na(cat@answers)) != 0){
+    stop("Cat object should not have respondent specific answers.")
+  }
+  
+  ans_profiles <- matrix(nrow = n, ncol = length(cat@answers))
+  for(respondent in 1:n){
+    for(i in 1:length(cat@answers)){
+      probs <- probability(catObj = cat, theta = theta, item = i)
+      
+      ## need to calculate answer probabilities from cumulative probabilities
+      if(cat@model == "grm"){
+        probs <- diff(probs)
+      }
+      ## need to append probability of answering a 0
+      if(cat@model == "ltm" | cat@model == "tpm"){
+        probs <- c(1 - probs, probs)
+      }
+      ## gpcm is fine
+      
+      ## now generate answers with those probabilities
+      ans_profiles[respondent, i] <- sample(1:(length(cat@difficulty[[i]])+1), 1, prob = probs)
+    }
+  }
+  return(as.data.frame(ans_profiles))
+}
+
+## LTM: A check with theta of 1, item 10
+probs <- probability(ltm_cat, 1, 10)
+sims <- simulateRespondents_test(ltm_cat, 1, 5000)
+recover_probs <- table(sims$V10)/nrow(sims)
+
+## GRM: A check with theta of 1, item 10
+probs <- diff(probability(grm_cat, 1, 10))
+sims <- simulateRespondents_test(grm_cat, 1, 5000)
+recover_probs <- table(sims$V10)/nrow(sims)
+
+## GPCM: A check with theta of 1, item 10
+probs <- probability(gpcm_cat, 1, 10)
+sims <- simulateRespondents_test(gpcm_cat, 1, 5000)
+recover_probs <- table(sims$V10)/nrow(sims)
+
+
+test_that("GRM returns correct output", {
+})
+
+test_that("...", {
+})
+
+test_that("Errors are thrown due to bad input", {
+})
