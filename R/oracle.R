037afd1bc8e03467bb1a985dacc37155a9739398
erossiter <erinrossiter@psci017als17.wustl.edu>
2018-06-07 15:45:25 -0500

    Oracle is done.
    - needs a test file
    - commented out the warning for now

--- R/oracle.R
+++ R/oracle.R
@@ -5,18 +5,20 @@
 #'
 #' @param catObj An object of class \code{Cat}
 #' @param theta A numeric representing the true position on the latent trait.
-#' @param ans_profile A vector representing the respondent's full answer profile.
+#' @param ans_profiles A vector representing the respondent's full answer profile.
 #' @param n A numeric indicating the length the combinations of all subsetted answer profile should be.
 #'
 #' @details ....
 #'
-#' @return ...
+#' @return A data.frame where the first column is the user-supplied true value of theta, the second column is the
+#' best possible theta estimate given n questions are asked, and the remaining columns are the answer profile leading
+#' to the best possible theta estimation.
 #'  
 #' @author Haley Acevedo, Ryden Butler, Josh W. Cutler, Matt Malis, Jacob M. Montgomery, Tom Wilkinson, Erin Rossiter, Min Hee Seo, Alex Weil 
 #' 
 #'
 #' @importFrom utils combn
-#'
+#' @importFrom plyr adply
 #' 
 #' @name oracle
 NULL
@@ -26,61 +28,46 @@ setGeneric("oracle", function(catObj, theta, ans_profiles, n) standardGeneric("o
 #' @rdname oracle
 #' @export
 setMethod(f = "oracle", signature = "Cat", definition = function(catObj, theta, ans_profiles, n){
-    ## matrix of answer profiles
-    ## vector of thetas
-    ## same 'n'
-    ## warning if n>5 or something...
-  
-  if(length(theta) != nrow(ans_profiles)){
-    stop("Need a corresponding theta value for each answe profile.")
-  }
-
-  if(length(ans_profiles) != length(catObj@answers)){
-    stop("Response profile is not compatible with Cat object.")
-  }
+    if(length(theta) != nrow(ans_profiles)){
+        stop("Need a corresponding theta value for each answer profile.")
+    }
     
-  ncombos <- choose(length(ans_profiles), n)
-  if(ncombos > 1000000){
-    stop("Too many combinations result from choose(length(ans_profiles), n).")
-  }
-  
-  if(n > 6){
-    warning("Will likely get to be arbitrarily close to the truth....")
-  }
-  
-  ## matrix of all length n combinations of ~indexes~
-  ## where each column is a possible combo
-  combo_mat <- combn(1:ncol(ans_profiles), n)
-  
-  ## results for one profile with true theta
-  find_truth <- function(t, ans, combo_mat = combo_mat){
-    # combo_profiles <- matrix(nrow = ncol(combo_mat),
-    #                          ncol = (length(catObj@answers)+1),
-    #                          dimnames = list(1:ncol(combo_mat),
-    #                                        c(paste0("q", 1:length(catObj@answers)), "theta_est")))
-    # for(i in 1:ncol(combo_mat)){
-    #   ## now use indexes to get actual answer profile
-    #   indexes <- combo_mat[ ,i]
-    #   combo_profiles[i, indexes] <- as.numeric(ans[indexes])
-    #   
-    #   catObj@answers <- combo_profiles[i, 1:(ncol(combo_profiles)-1)]
-    #   combo_profiles[i, "theta_est"] <- estimateTheta(catObj)
+    if(ncol(ans_profiles) != length(catObj@answers)){
+        stop("Response profiles are not compatible with Cat object.")
+    }
+    
+    ncombos <- choose(length(ans_profiles), n)
+    if(ncombos > 1000000){
+        stop("Too many combinations result from choose(length(ans_profiles), n).")
+    }
+    
+    # if(n > 5){
+    #     warning("Asking n>5 questions will likely be arbitrarily close...")
     # }
-    combo_profiles <- adply(.data = combo_mat, .margins = 2, .fun = function(indices, catObj = ltm_cat, ans = ans){
-      catObj@answers[indexes] <- ans[indexes]
-      theta_est <-  estimateTheta(catObj)
-      return(c(as.numeric(ans[indexes]), theta_est))
+  
+  ## matrix of all length n combinations of indexes where each row is a possible combo
+  combo_mat <- t(combn(1:ncol(ans_profiles), n))
+  
+  ## results for one profile with respective true theta
+  find_truth <- function(ind_theta, ind_ans, combo_mat, cat){
+    combo_profiles <- adply(.data = combo_mat,
+                            .margins = 1,
+                            .id = NULL,
+                            .fun = function(indices, catObj = cat, ans = ind_ans){
+                                catObj@answers[indices] <- unlist(ans[indices])
+                                theta_est <- estimateTheta(catObj)
+                                return(data.frame(theta = ind_theta, theta_est = theta_est, ans[indices]))
     })
     
-    return_row <- which.min(abs(combo_profiles[,"theta_est"] - t))
+    return_row <- which.min(abs(combo_profiles$theta_est - ind_theta))
     
-    return(c(t,
-             combo_profiles[return_row, "theta_est"],
-             combo_profiles[return_row, -ncol(combo_profiles)]))
+    return(combo_profiles[return_row, ])
   }
+  
+  ## results for each profile
   return(adply(.data = 1:nrow(ans_profiles),
                .margins = 1,
-               .fun = function(x) find_truth(t = theta[x], ans = ans_profiles[x,]),
+               .fun = function(x) find_truth(ind_theta = theta[x], ind_ans = ans_profiles[x,], combo_mat = combo_mat, cat = catObj),
                .id = NULL))
 })
 
