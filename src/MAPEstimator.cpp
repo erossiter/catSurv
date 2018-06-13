0c608e30d67a2753ec139cb24a1e4c511659648a
erossiter <erinrossiter@psci017als17.wustl.edu>
2018-06-13 09:34:03 -0500

    Starting to fix MAP starting value

--- src/MAPEstimator.cpp
+++ src/MAPEstimator.cpp
@@ -3,20 +3,39 @@ using namespace Rcpp;
 #include "MAPEstimator.h"
 
 double MAPEstimator::estimateTheta(Prior prior) {
-  int iter = 0;
-  int max_iter = 200;
-  
+    
+    
 	double theta_hat_old = 0.0;
 	double theta_hat_new = 1.0;
-
-	const double tolerance = 0.0000001;
-
 	double difference = std::abs(theta_hat_new - theta_hat_old);
 	
+	// check to see that the difference is not increasing!
+	std::vector<double> check_diffs;
+	for(int i = 1; i < 4; i++) {
+	    theta_hat_new = theta_hat_old - d1LL(theta_hat_old, true, prior) / d2LL(theta_hat_old, true, prior);
+	    difference = std::abs(theta_hat_new - theta_hat_old);
+	    check_diffs.push_back(difference);
+	    theta_hat_old = theta_hat_new;
+	}
+	if(check_diffs[1] > check_diffs[0]){ //} || check_diffs[2] > check_diffs[1]){ //difference shows we aren't "climbing" the likelihood
+	    theta_hat_old = 2.0; //estimate me!!;
+	    theta_hat_new = 1.0; //estimate me + 1???
+	    difference = std::abs(theta_hat_new - theta_hat_old); 
+	}else{ //start over
+	    theta_hat_old = 0.0;
+	    theta_hat_new = 1.0;
+	    difference = std::abs(theta_hat_new - theta_hat_old); 
+	}
+	
+	// now complete Newton-Raphson
+	int iter = 0;
+	int max_iter = 200;
+	const double tolerance = 0.0000001;
 	while (difference > tolerance && iter < max_iter) {
-	  iter++;
+	    iter++;
 		theta_hat_new = theta_hat_old - d1LL(theta_hat_old, true, prior) / d2LL(theta_hat_old, true, prior);
 		difference = std::abs(theta_hat_new - theta_hat_old);
+		std::cout<<difference<<std::endl;
 		theta_hat_old = theta_hat_new;
 	}
 	
--- src/MAPEstimator.h
+++ src/MAPEstimator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Estimator.h"
 #include "Prior.h"
+#include "EAPEstimator.h"
 
 
 class MAPEstimator : public Estimator {
@@ -16,5 +17,8 @@ public:
 	
 	virtual double estimateSE(Prior prior) override;
 	virtual double estimateSE(Prior prior, size_t question, int answer) override;
-
+	
+	// virtual double makeEAP(EAPEstimator* eapEstimator, Prior prior){
+	//     return eapEstimator->estimateTheta(prior);
+	// }
 };
