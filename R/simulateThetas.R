067bd41e0da0d05801e07eb82e6264af86ffe05b
erossiter <erinrossiter@psci017als17.wustl.edu>
2019-06-26 09:46:53 -0500

    Reconciling allEst and simulateThetas

--- R/simulateThetas.R
+++ R/simulateThetas.R
@@ -3,7 +3,7 @@
 #' Takes in response profiles from multiple respondents and multiple Cat object (i.e., adaptive battery) specifications and returns a set of theta estimates
 #'
 #' @param catObjs A list of \code{Cat} objects of the same model with different adaptive battery specifications
-#' @param resp A matrix of response profiles
+#' @param responses A matrix of response profiles
 #'
 #' @details The function takes multiple \code{Cat} objects, stored in a list, and generates an estimation for \code{theta}.
 #' 
@@ -36,32 +36,32 @@
 #' grmList <- list(grm_MAP, grm_EAP)
 #' 
 #' # Results
-#' theta_est_results <- allEst(catObjs = grmList, resp = respondents)
+#' theta_est_results <- simulateThetas(catObjs = grmList, responses = respondents)
 #' 
 #' @author Haley Acevedo, Ryden Butler, Josh W. Cutler, Matt Malis, Jacob M. Montgomery, Tom Wilkinson, Erin Rossiter, Min Hee Seo, Alex Weil, Jaerin Kim, Dominique Lockett 
 #' 
-#' @rdname allEst
+#' @rdname simulateThetas
 #' 
 #' @export
-allEst <- function(catObjs, resp){
-    UseMethod("allEst", catObj)
+simulateThetas <- function(catObjs, responses){
+    UseMethod("simulateThetas", catObj)
 }
 
-allEst<- function(catObjs=list(), resp){
+simulateThetas<- function(catObjs=list(), responses){
     
     # checks
     if(length(unique(lapply(catObjs, function(x) x@model))) != 1){
         stop("Cat objects must be of the same model e.g., grm.")
     }
-    if(any(unlist(lapply(catObjs, function(x) length(x@answers) != length(resp))))){
+    if(any(unlist(lapply(catObjs, function(x) length(x@answers) != length(responses))))){
         stop("Response profile is not compatible with Cat object.")
     }
     
     
     # for loop for now
-    out <- matrix(NA, nrow = nrow(resp), ncol = length(catObjs))
+    out <- matrix(NA, nrow = nrow(responses), ncol = length(catObjs))
     for (i in 1:length(catObjs)){
-        out[,i] <- apply(X = resp,
+        out[,i] <- apply(X = responses,
                             MARGIN = 1,
                             FUN = function(x, catObj){
                                 cat <- catObj
